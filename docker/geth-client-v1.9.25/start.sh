16a47b74143886b844248120572ceabcd647f806
cuonghx-gutech <cuonghx@gu-tech.com>
2022-05-12 10:09:04 +0700

    .

--- docker/geth-client-v1.9.25/start.sh
+++ docker/geth-client-v1.9.25/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sleep 3
-geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+geth --datadir=~/.ethereum/devchain init "/root/files/poa.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
